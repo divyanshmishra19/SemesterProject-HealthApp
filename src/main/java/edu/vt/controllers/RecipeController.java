76a2dedc116b73d632512b757bbaae40ae9ea3ed
Ayush Shah <ayushmanish@vt.edu>
2021-11-22 16:22:46 -0500

    Adding list page

--- src/main/java/edu/vt/controllers/RecipeController.java
+++ src/main/java/edu/vt/controllers/RecipeController.java
@@ -1,4 +1,26 @@
 package edu.vt.controllers;
 
-public class RecipeController {
+import edu.vt.EntityBeans.Recipe;
+import edu.vt.FacadeBeans.RecipeFacade;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+
+@Named("recipeController")
+@RequestScoped
+
+public class RecipeController implements Serializable {
+
+    private List<Recipe> listOfRecipes;
+    private RecipeFacade recipeFacade;
+
+
+    public List<Recipe> getListOfVideos() {
+        if (listOfRecipes == null) {
+            listOfRecipes = recipeFacade.findAll();
+        }
+        return listOfRecipes;
+    }
 }
