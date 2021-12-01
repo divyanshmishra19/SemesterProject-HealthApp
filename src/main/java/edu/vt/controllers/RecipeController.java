91acd5c024733df95344f3c5511b600e13e992b7
Divyansh Mishra <divyanshmishraalt@gmail.com>
2021-12-01 00:50:03 -0500

    Controllers for public recipe and workout updated

--- src/main/java/edu/vt/controllers/RecipeController.java
+++ src/main/java/edu/vt/controllers/RecipeController.java
@@ -1,17 +1,18 @@
 package edu.vt.controllers;
 
 import edu.vt.EntityBeans.Recipe;
+import edu.vt.EntityBeans.User;
+import edu.vt.EntityBeans.UserRecipe;
 import edu.vt.FacadeBeans.RecipeFacade;
+import edu.vt.FacadeBeans.UserRecipeFacade;
 import edu.vt.globals.Methods;
 
-import javax.ejb.EJBException;
+import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import java.io.Serializable;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import edu.vt.controllers.util.JsfUtil;
 
 @Named("recipeController")
 @RequestScoped
@@ -19,9 +20,15 @@ import edu.vt.controllers.util.JsfUtil;
 public class RecipeController implements Serializable {
 
     private List<Recipe> listOfRecipes;
-    private boolean recipeDataChanged;
-    private RecipeFacade recipeFacade;
     private Recipe selected;
+    private UserRecipe userRecipe;
+
+    @EJB
+    private RecipeFacade recipeFacade;
+
+    @EJB
+    private UserRecipeFacade userRecipeFacade;
+
 
     public Recipe getSelected() {
         return selected;
@@ -33,11 +40,38 @@ public class RecipeController implements Serializable {
 
     public List<Recipe> getListOfRecipes() {
         if (listOfRecipes == null) {
-            //listOfRecipes = recipeFacade.findAll();
+            listOfRecipes = recipeFacade.findAll();
         }
         return listOfRecipes;
     }
 
+    public void setListOfRecipes(List<Recipe> listOfRecipes) {
+        this.listOfRecipes = listOfRecipes;
+    }
+
+    public UserRecipe getUserRecipe() {
+        return userRecipe;
+    }
+
+    public void setUserRecipe(UserRecipe userRecipe) {
+        this.userRecipe = userRecipe;
+    }
+
+    public RecipeFacade getRecipeFacade() {
+        return recipeFacade;
+    }
+
+    public void setRecipeFacade(RecipeFacade recipeFacade) {
+        this.recipeFacade = recipeFacade;
+    }
+
+    public UserRecipeFacade getUserRecipeFacade() {
+        return userRecipeFacade;
+    }
+
+    public void setUserRecipeFacade(UserRecipeFacade userRecipeFacade) {
+        this.userRecipeFacade = userRecipeFacade;
+    }
 
     public void unselect() {
         selected = null;
@@ -46,126 +80,29 @@ public class RecipeController implements Serializable {
     public String cancel() {
         // Unselect previously selected movie object if any
         selected = null;
-        return "/videos/List?faces-redirect=true";
+        return "/recipe/List?faces-redirect=true";
     }
 
+    public void share() {
+        userRecipe = new UserRecipe();
+        User signedIn = (User) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("user");
+        if(signedIn==null)
+        {
+            Methods.showMessage("Information", "Unable to Share!", "To share a recipe, user must be logged in");
+            return;
+        }
 
-    public void prepareCreate() {
-        /*
-        Instantiate a new Video object and store its object reference into
-        instance variable 'selected'. The Movie class is defined in PublicVideo.java
-         */
-        selected = new Recipe();
-    }
+        userRecipe.setCalcium(selected.getCalcium());
+        userRecipe.setCalories(selected.getCalories());
+        userRecipe.setCarbCal(selected.getCarbCal());
+        userRecipe.setCarbs(selected.getCarbs());
+        userRecipe.setDietLabels(selected.getDietLabels());
+        userRecipe.setFatCal(selected.getFatCal());
+        //and all the rest of the fields..........
+        userRecipe.setUserId(signedIn);
 
-
-    public void create() {
         Methods.preserveMessages();
-
-        persist(JsfUtil.PersistAction.CREATE,"Public Video was successfully created.");
-
-        if (!JsfUtil.isValidationFailed()) {
-            // No JSF validation error. The CREATE operation is successfully performed.
-            selected = null;        // Remove selection
-            listOfRecipes = null;    // Invalidate listOfMovies to trigger re-query.
-            recipeDataChanged = true;
-        }
+        userRecipeFacade.edit(userRecipe);
+        Methods.showMessage("Information", "Shared!", "Public recipe was successfully shared.");
     }
-
-    public void update() {
-        Methods.preserveMessages();
-
-        persist(JsfUtil.PersistAction.UPDATE,"Public Video was successfully updated.");
-
-        if (!JsfUtil.isValidationFailed()) {
-            // No JSF validation error. The UPDATE operation is successfully performed.
-            selected = null;        // Remove selection
-            listOfRecipes = null;    // Invalidate listOfMovies to trigger re-query.
-        }
-    }
-
-    public void destroy() {
-        Methods.preserveMessages();
-
-        persist(JsfUtil.PersistAction.DELETE,"Public Video was successfully deleted.");
-
-        if (!JsfUtil.isValidationFailed()) {
-            // No JSF validation error. The DELETE operation is successfully performed.
-            selected = null;        // Remove selection
-            listOfRecipes = null;    // Invalidate listOfMovies to trigger re-query.
-        }
-    }
-
-    /*
-     **********************************************************************************************
-     *   Perform CREATE, UPDATE (EDIT), and DELETE (DESTROY, REMOVE) Operations in the Database   *
-     **********************************************************************************************
-     */
-    /**
-     * @param persistAction refers to CREATE, UPDATE (Edit) or DELETE action
-     * @param successMessage displayed to inform the user about the result
-     */
-    private void persist(JsfUtil.PersistAction persistAction, String successMessage) {
-        if (selected != null) {
-            try {
-                if (persistAction != JsfUtil.PersistAction.DELETE) {
-                    /*
-                     -------------------------------------------------
-                     Perform CREATE or EDIT operation in the database.
-                     -------------------------------------------------
-                     The edit(selected) method performs the SAVE (STORE) operation of the "selected"
-                     object in the database regardless of whether the object is a newly
-                     created object (CREATE) or an edited (updated) object (EDIT or UPDATE).
-
-                     PublicVideoFacade inherits the edit(selected) method from the AbstractFacade class.
-                     */
-                    recipeFacade.edit(selected);
-                } else {
-                    /*
-                     -----------------------------------------
-                     Perform DELETE operation in the database.
-                     -----------------------------------------
-                     The remove(selected) method performs the DELETE operation of the "selected"
-                     object in the database.
-
-                     PublicVideoFacade inherits the remove(selected) method from the AbstractFacade class.
-                     */
-                    recipeFacade.remove(selected);
-                }
-                JsfUtil.addSuccessMessage(successMessage);
-            } catch (EJBException ex) {
-                String msg = "";
-                Throwable cause = ex.getCause();
-                if (cause != null) {
-                    msg = cause.getLocalizedMessage();
-                }
-                if (msg.length() > 0) {
-                    JsfUtil.addErrorMessage(msg);
-                } else {
-                    JsfUtil.addErrorMessage(ex,"A persistence error occurred.");
-                }
-            } catch (Exception ex) {
-                Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
-                JsfUtil.addErrorMessage(ex,"A persistence error occurred.");
-            }
-        }
-    }
-
-    /*
-     ******************************************
-     *   Display the Search Results JSF Page  *
-     ******************************************
-     */
-//    public String search(Integer type) {
-//        // Set search type given as input parameter
-//        searchType = type;
-//
-//        // Unselect previously selected video if any before showing the search results
-//        selected = null;
-//
-//        // Invalidate list of search items to trigger re-query.
-//        searchItems = null;
-//
-//        return "/publicVideo/SearchResults?faces-redirect=true";
-//    }
 }
--- src/main/java/edu/vt/controllers/WorkoutController.java
+++ src/main/java/edu/vt/controllers/WorkoutController.java
@@ -1,20 +1,16 @@
 package edu.vt.controllers;
 
-import edu.vt.EntityBeans.Recipe;
-import edu.vt.EntityBeans.Workout;
+import edu.vt.EntityBeans.*;
+import edu.vt.FacadeBeans.UserWorkoutFacade;
 import edu.vt.FacadeBeans.WorkoutFacade;
-import edu.vt.controllers.util.JsfUtil;
 import edu.vt.globals.Methods;
 
-import javax.ejb.EJBException;
+import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 
 @Named("workoutController")
 @RequestScoped
@@ -22,8 +18,15 @@ import java.util.logging.Logger;
 public class WorkoutController implements Serializable {
 
     private List<Workout> listOfWorkouts;
-    private WorkoutFacade workoutFacade;
     private Workout selected;
+    private UserWorkout userWorkout;
+    private String youtubeTutorialVideoId;
+
+    @EJB
+    private WorkoutFacade workoutFacade;
+
+    @EJB
+    private UserWorkoutFacade userWorkoutFacade;
 
     public Workout getSelected() {
         return selected;
@@ -35,131 +38,70 @@ public class WorkoutController implements Serializable {
 
     public List<Workout> getListOfWorkouts() {
         if (listOfWorkouts == null) {
-            List<Workout> wlist = new ArrayList<>();
-            /*
-            wlist.add(new Workout(5.0,"Pull Ups", "Strength"));
-            wlist.add(new Workout(0.5,"Walking", "Aerobic"));
-            wlist.add(new Workout(2,"Jogging", "Aerobic"));
-
-             */
-            return wlist;
+            listOfWorkouts = workoutFacade.findAll();
         }
         return listOfWorkouts;
     }
 
-    public String getUrlOfVideoToPlay () {
-        return getUrlOfVideoToPlay();
+    public void setListOfWorkouts(List<Workout> listOfWorkouts) {
+        this.listOfWorkouts = listOfWorkouts;
     }
 
-    public void setUrlOfVideoToPlay(String urlOfVideoToPlay) {
-        selected.setYoutubeTutorialVideoId(urlOfVideoToPlay);
+    public UserWorkout getUserWorkout() {
+        return userWorkout;
+    }
+
+    public void setUserWorkout(UserWorkout userWorkout) {
+        this.userWorkout = userWorkout;
+    }
+
+    public String getYoutubeTutorialVideoId() {
+        return youtubeTutorialVideoId;
+    }
+
+    public void setYoutubeTutorialVideoId(String youtubeTutorialVideoId) {
+        selected.setYoutubeTutorialVideoId(youtubeTutorialVideoId);
+    }
+
+    public WorkoutFacade getWorkoutFacade() {
+        return workoutFacade;
+    }
+
+    public void setWorkoutFacade(WorkoutFacade workoutFacade) {
+        this.workoutFacade = workoutFacade;
+    }
+
+    public UserWorkoutFacade getUserWorkoutFacade() {
+        return userWorkoutFacade;
+    }
+
+    public void setUserWorkoutFacade(UserWorkoutFacade userWorkoutFacade) {
+        this.userWorkoutFacade = userWorkoutFacade;
     }
 
     public String cancel() {
         // Unselect previously selected movie object if any
         selected = null;
-        return "/videos/List?faces-redirect=true";
+        return "/workout/List?faces-redirect=true";
     }
 
+    public void share() {
+        userWorkout = new UserWorkout();
+        User signedIn = (User) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("user");
+        if(signedIn==null)
+        {
+            Methods.showMessage("Information", "Unable to Share!", "To share a workout routine, user must be logged in");
+            return;
+        }
 
-    public void prepareCreate() {
-        /*
-        Instantiate a new Video object and store its object reference into
-        instance variable 'selected'. The Movie class is defined in PublicVideo.java
-         */
-        selected = new Workout();
-    }
+        userWorkout.setYoutubeTutorialVideoId(selected.getYoutubeTutorialVideoId());
+        userWorkout.setBurnRate(selected.getBurnRate());
+        userWorkout.setCategory(selected.getCategory());
+        userWorkout.setName(selected.getName());
+        userWorkout.setUserId(signedIn);
 
-
-    public void create() {
         Methods.preserveMessages();
-
-        persist(JsfUtil.PersistAction.CREATE,"Public Video was successfully created.");
-
-        if (!JsfUtil.isValidationFailed()) {
-            // No JSF validation error. The CREATE operation is successfully performed.
-            selected = null;        // Remove selection
-            listOfWorkouts = null;    // Invalidate listOfMovies to trigger re-query.
-
-        }
-    }
-
-    public void update() {
-        Methods.preserveMessages();
-
-        persist(JsfUtil.PersistAction.UPDATE,"Public Video was successfully updated.");
-
-        if (!JsfUtil.isValidationFailed()) {
-            // No JSF validation error. The UPDATE operation is successfully performed.
-            selected = null;        // Remove selection
-            listOfWorkouts = null;    // Invalidate listOfMovies to trigger re-query.
-        }
-    }
-
-    public void destroy() {
-        Methods.preserveMessages();
-
-        persist(JsfUtil.PersistAction.DELETE,"Public Video was successfully deleted.");
-
-        if (!JsfUtil.isValidationFailed()) {
-            // No JSF validation error. The DELETE operation is successfully performed.
-            selected = null;        // Remove selection
-            listOfWorkouts = null;    // Invalidate listOfMovies to trigger re-query.
-        }
-    }
-
-    /*
-     **********************************************************************************************
-     *   Perform CREATE, UPDATE (EDIT), and DELETE (DESTROY, REMOVE) Operations in the Database   *
-     **********************************************************************************************
-     */
-    /**
-     * @param persistAction refers to CREATE, UPDATE (Edit) or DELETE action
-     * @param successMessage displayed to inform the user about the result
-     */
-    private void persist(JsfUtil.PersistAction persistAction, String successMessage) {
-        if (selected != null) {
-            try {
-                if (persistAction != JsfUtil.PersistAction.DELETE) {
-                    /*
-                     -------------------------------------------------
-                     Perform CREATE or EDIT operation in the database.
-                     -------------------------------------------------
-                     The edit(selected) method performs the SAVE (STORE) operation of the "selected"
-                     object in the database regardless of whether the object is a newly
-                     created object (CREATE) or an edited (updated) object (EDIT or UPDATE).
-
-                     PublicVideoFacade inherits the edit(selected) method from the AbstractFacade class.
-                     */
-                    workoutFacade.edit(selected);
-                } else {
-                    /*
-                     -----------------------------------------
-                     Perform DELETE operation in the database.
-                     -----------------------------------------
-                     The remove(selected) method performs the DELETE operation of the "selected"
-                     object in the database.
-
-                     PublicVideoFacade inherits the remove(selected) method from the AbstractFacade class.
-                     */
-                    workoutFacade.remove(selected);
-                }
-                JsfUtil.addSuccessMessage(successMessage);
-            } catch (EJBException ex) {
-                String msg = "";
-                Throwable cause = ex.getCause();
-                if (cause != null) {
-                    msg = cause.getLocalizedMessage();
-                }
-                if (msg.length() > 0) {
-                    JsfUtil.addErrorMessage(msg);
-                } else {
-                    JsfUtil.addErrorMessage(ex,"A persistence error occurred.");
-                }
-            } catch (Exception ex) {
-                Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
-                JsfUtil.addErrorMessage(ex,"A persistence error occurred.");
-            }
-        }
+        userWorkoutFacade.edit(userWorkout);
+        Methods.showMessage("Information", "Shared!", "Public workout routine was successfully shared.");
     }
 }
